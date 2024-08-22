0437a4fce97fcd40d84de7ec656311afbae1672b
Douglas Santos <Douglassantos6@outlook.com>
2024-08-22 09:31:03 -0300

    Definindo o relacionamento das classes modelo

--- MyFinance.Core/Domains/Divida.cs
+++ MyFinance.Core/Domains/Divida.cs
@@ -4,7 +4,7 @@ namespace MyFinance.Core.Domains;
 
 public class Divida : Base
 {
-    public string descricao { get; set; } = string.Empty;
-    public DateTime data { get; set; } = DateTime.MinValue;
-    public DateTime dataValidade { get; set; } = DateTime.MinValue;
+    public string Descricao { get; set; } = string.Empty;
+    public DateTime Data { get; set; } = DateTime.MinValue;
+    public DateTime DataValidade { get; set; } = DateTime.MinValue;
 }
