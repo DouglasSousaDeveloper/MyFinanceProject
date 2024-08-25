3a42b5c59610ec9baa0e88306a1cb55edc61c0c0
Douglas Santos <Douglassantos6@outlook.com>
2024-08-25 20:40:15 -0300

    fix

--- MyFinance.Api/GlobalUsings.cs
+++ MyFinance.Api/GlobalUsings.cs
@@ -1,7 +1,7 @@
 ﻿//Usings global que serão usados em todo o assembly
 global using MediatR;
 global using Microsoft.AspNetCore.Mvc;
-global using MyFinance.Application.Commands.User.Create;
+global using MyFinance.Application.Commands.User;
 
 namespace MyFinance.Api;
 
