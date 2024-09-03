d2ddb0d2020d780895983b0010bc6dc8170dc4af
Douglas Santos <Douglassantos6@outlook.com>
2024-09-03 17:49:13 -0300

    Criando AccountUpdateHandler

---
+++ MyFinance.Application/Commands/Account/AccountUpdateCommand.cs
@@ -0,0 +1,6 @@
+﻿namespace MyFinance.Application.Commands.Account;
+
+public class AccountUpdateCommand : IRequest<Result>
+{
+    public string CPF { get; set; }
+}
---
+++ MyFinance.Application/Commands/Account/AccountUpdateHandler.cs
@@ -0,0 +1,10 @@
+﻿
+namespace MyFinance.Application.Commands.Account;
+
+internal class AccountUpdateHandler : IRequestHandler<AccountUpdateCommand, Result>
+{
+    public Task<Result> Handle(AccountUpdateCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
